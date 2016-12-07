4678038a8ff2b70e7355b7342b7282f90ff5fa93
bfr3688 <jackshamu@yahoo.com>
2016-12-07 14:52:19 -0800

    Add project files.

---
+++ Arduino_Datalogger/Arduino_Datalogger.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_Datalogger.ino
+ Created:	12/7/2016 2:50:46 PM
+ Author:	user
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
